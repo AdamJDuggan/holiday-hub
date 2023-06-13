93ebb9968a8daf3f42639362b08aee41dd2d064e
AdamJDuggan <adamduggan17@gmail.com>
2023-06-13 11:49:02 +0100

    building out survery table

--- client/src/components/Landing/index.tsx
+++ client/src/components/Landing/index.tsx
@@ -2,18 +2,20 @@
 import React from "react";
 // Components
 import Button from "../Button";
-// Styles
-import styles from "../../styles/index.scss";
 
 export default function Landing() {
   const [pending, setPending] = React.useState(false);
   return (
-    <main className={"fill-screen grid place-items-center landing py-24"}>
+    <main
+      className={"fill-screen grid place-items-center landing py-24 md:py-12"}
+    >
       <div className="container">
-        <div className="grid gap-24 md:grid-cols-2 items-center">
+        <div className="grid gap-24 md:grid-cols-2 items-start">
           <div>
-            <h1 className="text-5xl mb-8 font-bold">Holiday Hub</h1>
-            <p className="text-xl mb-8">
+            <h1 className="text-6xl sm:text-7xl md:text-8xl mb-8 md:mb-10 font-bold">
+              Holiday Hub
+            </h1>
+            <p className="text-xl sm:text-1xl md:text-2xl mb-8 md:mb-10">
               Create groups, plan holidays and share memories.
             </p>
             <div className="flex space-x-4">
--- client/src/components/Survey/index.tsx
+++ client/src/components/Survey/index.tsx
@@ -1,23 +1,78 @@
-import React from "react";
+// 3rd party
+import classnames from "classnames";
+// Models
+import SurveyOption from "../../models/SurveyOption";
+// Styles
+import styles from "./index.module.scss";
 
 const optionOne = {
-  title: "Title",
-  start: Date.now(),
+  id: "option1",
+  start: new Date(),
   end: new Date("June 20, 2023 11:00"),
   description: "This is our prefered option",
+  accept: ["user1"],
+};
+const optionTwo = {
+  id: "option2",
+  title: "Option 2",
+  start: new Date(),
+  end: new Date("June 20, 2023 11:00"),
+  accept: ["user1", "user2"],
 };
 
-const optionOne = {
-  start: Date.now(),
-  end: new Date("June 20, 2023 11:00"),
-  description: null,
-};
+interface Member {
+  id: string;
+  name: string;
+}
+interface Option {
+  id: string;
+  title?: string;
+  start?: Date;
+  end?: Date;
+  description?: string;
+  accept: string[];
+}
+
+interface Survey {
+  // Hub id
+  hub: string;
+  // User ids of group members who can control the survey
+  admin: string[];
+  options: Option[];
+}
+
+interface Hub {
+  id: string;
+  admins: string[];
+  members: Member[];
+  surverys: Survey[];
+}
+
+const options: Option[] = [optionOne, optionTwo];
+
+const members: Member[] = [
+  { id: "user1", name: "Karin" },
+  { id: "user2", name: "ChunLi" },
+];
+
+const classes = [];
 
 export default function Survey() {
   return (
-    <table>
-      <th></th>
-      <tb></tb>
-    </table>
+    <div className="grid place-items-center">
+      <div className="container">
+        <table>
+          <th>
+            <tr>
+              <th>Member</th>
+              {options.map((option) => (
+                <th key={option.id}>{option.title || "Date"}</th>
+              ))}
+            </tr>
+          </th>
+          <tb></tb>
+        </table>
+      </div>
+    </div>
   );
 }
